688ba5fe26ff1541c5f9b007510dfd40d307e2a2
blackjackmk <kavalchukmaksum@gmail.com>
2024-02-14 20:56:40 +0100

    face recognition

--- securix.py
+++ securix.py
@@ -16,9 +16,10 @@ def face_capture():
         faces_alt = clf_alt.detectMultiScale(gray,1.1,5)
         faces_profile = clfp.detectMultiScale(gray,1.1,5)
         bodies = clb.detectMultiScale(gray,1.1,5)
-
+        
         for (x, y, width, height) in faces:
             cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 0, 255), 1)
+            face_recogn(frame)
         for (x, y, width, height) in faces_profile:
             cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 200, 255), 1)
         for (x, y, width, height) in faces_alt:
@@ -26,26 +27,19 @@ def face_capture():
         for (x, y, w, h) in bodies:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-        if len(faces) > 0:
-            face_recogn(frame)
-
         cv2.imshow('Securix', frame)
         if cv2.waitKey(1) == ord('q'):#press q key to quit
             break
     camera.release()
     cv2.destroyAllWindows()
 
-def face_recogn(input_img):#face check
-    try:
-        result = DeepFace.find(input_img, db_path="users")
+def face_recogn(frame):#face check
+    result = DeepFace.find(frame, db_path="users")
+    if result:
+        print("Known person")
+    else:
+        print("Unknown person")
 
-        if result:
-            print("Access submit")
-        else:
-            print("Access denied")
-
-    except Exception as _ex:
-        return _ex
     
 
 def main():
